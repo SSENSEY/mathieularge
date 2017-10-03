88af595afce571ea7c38b8c2c07774cd43031ace
SSENSEY <zarkrey@gmail.com>
2017-10-03 20:57:12 +0200

    Envoi page work in progress

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$title = 'ML | Work in Progress.';
+?>
+
+
+<!doctype html>
+<html lang="fr">
+ <head>
+ 		<meta charset="UTF-8">
+ 		<title><?php echo $title; ?></title>
+ 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="img/manifest.json">
+    <link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+ </head>
+
+    <body>
+        <h1>WORK   IN   PROGRESS.</h1>
+    </body>
+
+    <video autoplay loop id="video-background" muted>
+      <source src="img/bg.mp4" type="video/mp4">
+    </video>
+
+
+</html>
