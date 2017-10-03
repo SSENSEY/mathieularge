718e93b1275a0c82d64f4913de50c53982ef8896
SSENSEY <zarkrey@gmail.com>
2017-10-03 22:51:03 +0200

    Correction Home

--- app/templates/parts/header.php
+++ app/templates/parts/header.php
@@ -1,3 +1,3 @@
 
-<a href="?page=home" class="menu_items <?php echo (!isset($_GET['page']) || $_GET['page'] == 'home') ? 'active' : null; ?>">à propos</a>
-<a href="?page=about" class="menu_items <?php echo (isset($_GET['page']) && $_GET['page'] == 'about' ) ? 'active' : null; ?>">About</a>
+<a href="?page=home" class="<?php echo (!isset($_GET['page']) || $_GET['page'] == 'home') ? 'active' : null; ?>">Home</a>
+<a href="?page=about" class="<?php echo (isset($_GET['page']) && $_GET['page'] == 'about' ) ? 'active' : null; ?>">About</a>
