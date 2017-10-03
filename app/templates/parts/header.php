aee4ac40b2c89f1d3401ee39e56c8f79008c6e4e
mathieu <mathieu.berard.web@gmail.com>
2017-10-03 21:53:24 +0200

    Configuration de l'architecture

---
+++ app/templates/parts/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jeff
+ * Date: 03/10/2017
+ * Time: 21:48
+ */
---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <title>Mathieu Large — Développeur & Webdesigner</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header class="page-header">
+        <?php include 'app/templates/parts/header.php'; ?>
+    </header>
+
+    <div class="main-container">
+
+    </div>
+
+    <footer class="page-footer">
+        <?php include 'app/templates/parts/footer.php'; ?>
+    </footer>
+
+    <script src="app/ressources/js/script.js"></script>
+</body>
+</html>
